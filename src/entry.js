15439cf01fed651923e0a5dd5a6ef01441b97bc9
Martijn Kunstman <martijn@kunstman.nl>
2023-03-24 22:10:56 +0100

    gol rules

--- src/entry.js
+++ src/entry.js
@@ -7,6 +7,57 @@
  *
  */
 
+/*
+
+game of life 2d rules
+
+1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+game of life 3d rules
+
+How many neighbors does a cell have in 3D?
+A cell has 26 neighbors in 3D. The 26 neighbors are the 6 cells that share a face with the cell, the 12 cells that share an edge with the cell, and the 8 cells that share a vertex with the cell.
+
+//rules 2d 
+
+
+
+alive
+00000000 0
+00000001 1
+00000010 2
+00000011 3
+00000100 4
+00000101 5
+00000110 6
+00000111 7
+00001000 8
+00001001 9
+00001010 10
+00001011 11
+00001100 12
+00001101 13
+00001110 14
+00001111 15
+00010000 16
+00010001 17
+00010010 18
+00010011 19
+
+
+
+
+
+
+
+
+
+
+
+*/
 
 import {
   WebGLRenderer,
@@ -28,19 +79,20 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 //import SeedScene from "./objects/Scene.js";
 
 //resolution
-let resolution = 5;
-let cubeSize = 0.8;
+let resolution = 15;
+let cubeSize = 0.5;
 
 let data = [];
 
 const scene = new Scene();
 scene.background = new Color(0x444444);
-scene.fog = new Fog(0x000000, 0, resolution * 4);
-const light = new AmbientLight( 0xff8888, 0.3 ); // soft white light
-scene.add( light );
-const directionalLight = new DirectionalLight( 0x99ff99, 0.9 );
-directionalLight.position.set( resolution/2, resolution/3, resolution/4);
-directionalLight.target.position.set( 0, 0, 0 );
+//scene.fog = new Fog(0x000000, 0, resolution * 4);
+const light = new AmbientLight(0xff8888, 0.3); // soft white light
+scene.add(light);
+const directionalLight = new DirectionalLight(0x99ff99, 0.9);
+directionalLight.position.set(resolution / 2, resolution / 3, resolution / 4);
+directionalLight.target.position.set(0, 0, 0);
+/*
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.width = 512;
 directionalLight.shadow.mapSize.height = 512;
@@ -48,9 +100,9 @@ directionalLight.shadow.camera.near = 0.5;
 directionalLight.shadow.camera.far = 500;
 directionalLight.shadow.radius = 10;
 directionalLight.shadow.blurSamples = 25;
+*/
 
-
-scene.add( directionalLight );
+scene.add(directionalLight);
 
 const camera = new PerspectiveCamera();
 
@@ -73,33 +125,89 @@ const cube = new Mesh(geometry, material);
 cube.scale.set(resolution, resolution, resolution);
 
 //scene.add(cube);
+let data3d = [];
 
 for (let x = 0; x < resolution; x++) {
   data.push([]);
+  data3d.push([]);
   for (let y = 0; y < resolution; y++) {
     data[x].push([]);
+    data3d[x].push([]);
     for (let z = 0; z < resolution; z++) {
       data[x][y].push(0);
+      data3d[x][y].push(Math.round(Math.random()));
       let geometry = new BoxGeometry(cubeSize, cubeSize, cubeSize);
       let material = new MeshPhongMaterial({
         color: 0xaaaaff,
         //wireframe: false,
-        fog: true,
+        //fog: true,
         //depthWrite: true,
       });
       let cube = new Mesh(geometry, material);
-      cube.castShadow = true;
-      cube.receiveShadow = true;
+      cube.castShadow = false;
+      cube.receiveShadow = false;
       cube.position.set(
         x - resolution / 2 + 0.5,
         y - resolution / 2 + 0.5,
         z - resolution / 2 + 0.5
       );
-      cube.scale.set(Math.random(), Math.random(), Math.random());
+
+      cube.scale.set(data3d[x][y][z], data3d[x][y][z], data3d[x][y][z]);
+
       scene.add(cube);
       data[x][y][z] = cube;
     }
   }
+}
+
+function golUpdate() {
+  let temp = [];
+  for (let x = 0; x < resolution; x++) {
+    temp[x] = [];
+    for (let y = 0; y < resolution; y++) {
+      temp[x][y] = [];
+      for (let z = 0; z < resolution; z++) {
+        temp[x][y][z] = data3d[x][y][z];
+        let neighbors = checkNeighbors(x, y, z);
+        if (data3d[x][y][z] == 1) {
+          if (neighbors < 2 || neighbors > 3) {
+            temp[x][y][z] = 0;
+          }
+        } else {
+          if (neighbors == 3) {
+            temp[x][y][z] = 1;
+          }
+        }
+      }
+    }
+  }
+  data3d = temp;
+}
+
+function checkNeighbors(x, y, z) {
+  let neighbors = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      for (let k = -1; k < 2; k++) {
+        if (i == 0 && j == 0 && k == 0) {
+          continue;
+        }
+        if (
+          x + i >= 0 &&
+          x + i < resolution &&
+          y + j >= 0 &&
+          y + j < resolution &&
+          z + k >= 0 &&
+          z + k < resolution
+        ) {
+          if (data3d[x + i][y + j][z + k] == 1) {
+            neighbors++;
+          }
+        }
+      }
+    }
+  }
+  return neighbors;
 }
 
 // scene
@@ -117,6 +225,7 @@ renderer.setClearColor(0x7ec0ee, 1);
 const onAnimationFrameHandler = (timeStamp) => {
   renderer.render(scene, camera);
   //seedScene.update && seedScene.update(timeStamp);
+  golUpdate();
   dataUpdate();
   controls.update();
   window.requestAnimationFrame(onAnimationFrameHandler);
@@ -127,11 +236,16 @@ const dataUpdate = () => {
   for (let x = 0; x < resolution; x++) {
     for (let y = 0; y < resolution; y++) {
       for (let z = 0; z < resolution; z++) {
-        let cube = data[x][y][z];
-        cube.rotation.x += 0.001;
-        cube.rotation.y += 0.001;
-        cube.rotation.z += 0.001;
+        //let cube = data[x][y][z];
+        //cube.rotation.x += 0.001;
+        //cube.rotation.y += 0.001;
+        //cube.rotation.z += 0.001;
         //data[x][y][z].scale.set(Math.random(), Math.random(), Math.random());
+        data[x][y][z].scale.set(
+          data3d[x][y][z],
+          data3d[x][y][z],
+          data3d[x][y][z]
+        );
       }
     }
   }
